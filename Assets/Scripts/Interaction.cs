a939ac0a8033bb743268ff6ceed5ec12de228599
AdrianH2003 <drnheffley@gmail.com>
2022-07-14 16:14:01 -0400

    Removed Start and Update from Interaction Script for Simplicity

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -22,15 +22,4 @@ public class Interaction : MonoBehaviour
             yield return new WaitUntil(()=> audiosource.isPlaying == false);
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
